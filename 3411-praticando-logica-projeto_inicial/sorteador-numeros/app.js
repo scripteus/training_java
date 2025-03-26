8856578d7f8da2dc3c50896622b9beffbb7df92d
@ScripTeus <scripteusdev@gmail.com>
2025-03-26 10:12:58 -0300

    Tudo certo

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,7 @@
+function sortear () {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    console.log(quantidade, de, ate);
+}
