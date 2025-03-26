a0698a8b3a04928ba059be621279a89424432e0e
@ScripTeus <scripteusdev@gmail.com>
2025-03-26 11:56:36 -0300

    Concluindo o botão de sortear

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,7 +1,62 @@
-function sortear () {
-    let quantidade = document.getElementById('quantidade').value;
-    let de = document.getElementById('de').value;
-    let ate = document.getElementById('ate').value;
+function gerarNumerosAleatorios(min, max) {
+    // Gera um número aleatório entre min e max
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
-    console.log(quantidade, de, ate);
+function sortear() {
+    
+    // Pega os valores dos inputs
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let numerosSorteados = [];
+
+    // Sorteia os números
+    for (let i = 0; i < quantidade; i++) {
+        let numeroSorteado = gerarNumerosAleatorios(de, ate);
+
+    Verifica se o número já foi sorteado
+    while (numerosSorteados.includes(numeroSorteado)) {
+        numeroSorteado = gerarNumerosAleatorios(de, ate);
+    }
+        numerosSorteados.push(numeroSorteado);
+    }
+
+    // if (ate > de) {
+    //     // Se o número "de" for maior que "ate" retorne um erro
+    //     return alert("O valor de 'de' não pode ser maior que o valor de 'ate');;
+    // } else {
+    //     // Se o número "de" for menor ou igual a "ate" retorne os números
+    //     let resultado = document.getElementById('resultado');
+    //     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numerosSorteados}</label>`;
+    //     alterarStatusDoBotao();
+    // }
+    // }
+
+    // Mostra o resultado na label "resultado"
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${numerosSorteados}</label>`;
+    alterarStatusDoBotao();
+}
+
+function alterarStatusDoBotao () {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '';
+    alterarStatusDoBotao();
 }
