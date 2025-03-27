9f4a8e70d045c6aa38a89ba4d0a7124d2a9c2db3
@ScripTeus <scripteusdev@gmail.com>
2025-03-27 14:04:35 -0300

    Testes
    Realizando alguns bem básicos no Intellij

---
+++ Java - create the first application/src/Soma.java
@@ -0,0 +1,9 @@
+public class Soma {
+    public Soma() {
+    }
+
+    public static void main(String[] args) {
+        int soma = 10 + 5;
+        System.out.println("A soma entre 10 e 5 é: " + soma);
+    }
+}
