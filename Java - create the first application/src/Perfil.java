a3856592046da5559742ce8bb7d5c5b81915cee6
@ScripTeus <scripteusdev@gmail.com>
2025-03-28 09:16:48 -0300

    Testes
    Realizando alguns bem básicos no Intellij

--- Java - create the first application/src/Perfil.java
+++ Java - create the first application/src/Perfil.java
@@ -4,6 +4,8 @@ public class Perfil {
 
 public static void main(String[] args) {
     String nome = "Matheus";
-        System.out.println("Olá, "+ nome +", tudo bem?");
+
+    System.out.println("Olá, "+ nome +", tudo bem?");
+
 }
 }
