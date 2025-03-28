e204b646af659c2fd310e341a41d52bb4fe393f4
@ScripTeus <scripteusdev@gmail.com>
2025-03-28 09:18:05 -0300

    Teste unitário

--- Java - create the first application/src/Perfil.java
+++ Java - create the first application/src/Perfil.java
@@ -6,6 +6,9 @@ public static void main(String[] args) {
     String nome = "Matheus";
 
     System.out.println("Olá, "+ nome +", tudo bem?");
-
+    System.out.println("Olá, "+ nome +", tudo bem?");
+    System.out.println("Olá, "+ nome +", tudo bem?");
+    System.out.println("Olá, "+ nome +", tudo bem?");
+    System.out.println("Olá, "+ nome +", tudo bem?");
 }
 }
