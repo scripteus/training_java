93bc4d6ca0724a248f37680137461c3f73b57455
@ScripTeus <scripteusdev@gmail.com>
2025-04-06 14:31:53 -0300

    Update Main.java

--- Trilha de Java/src/Main.java
+++ Trilha de Java/src/Main.java
@@ -2,7 +2,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
     public class Main {
-        public static void main(String[] args) {
+        public static void main(String[ ] args) {
 
             Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
